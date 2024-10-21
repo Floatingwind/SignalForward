3d4c16a14d1a781c91b7e1df145469bf677646b0
cz <czy190824@163.com>
2024-10-21 16:07:00 +0800

    .

--- SignalForwardUdp.cs
+++ SignalForwardUdp.cs
@@ -135,7 +135,30 @@ namespace SignalForward
             numericUpDown1.Enabled = false;
 
             InitParam();
+            CallOnClick(RemoteBnt);
+            CallOnClick(button1);
+            CallOnClick(button2);
+
             Task.Factory.StartNew(Remove, _tokenSource2.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+
+        /// <summary>
+        /// 触发按钮的点击事件
+        /// </summary>
+        /// <param name="btn"></param>
+        private void CallOnClick(Button btn)
+        {
+            //建立一个类型  
+            Type t = typeof(Button);
+            //参数对象  
+            object[] p = new object[1];
+            //产生方法  
+            MethodInfo m = t.GetMethod("OnClick", BindingFlags.NonPublic | BindingFlags.Instance);
+            //参数赋值。传入函数  
+            p[0] = EventArgs.Empty;
+            //调用  
+            m.Invoke(btn, p);
+            return;
         }
 
         #region 同步方式
